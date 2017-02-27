0342fb1f31d244d1466a5270d5aebdc078f750a7
Anton Fenske <anton@fenske.io>
2017-02-27 21:18:54 +0100

    Implement set of stacks.

---
+++ src/main/java/io/fenske/crackinginterview/chapter3/SetOfStacks.java
@@ -0,0 +1,64 @@
+package io.fenske.crackinginterview.chapter3;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class SetOfStacks {
+    private int activeStackIndex = 0;
+    private List<LinkedList<Integer>> stackList = new ArrayList<>();
+
+    private final int capacity;
+
+    public SetOfStacks(int capacity) {
+        this.capacity = capacity;
+        stackList.add(new LinkedList<>());
+    }
+
+    public void push(int element) {
+        if (activeStack().size() < capacity) {
+            activeStack().push(element);
+        } else {
+            expandAndPush(element);
+        }
+    }
+
+    private void expandAndPush(int element) {
+        stackList.add(new LinkedList<>());
+        ++activeStackIndex;
+        activeStack().push(element);
+    }
+
+    public int pop() {
+        if (activeStack().isEmpty()) {
+            throw new UnderflowException();
+        }
+        int poppedElement = activeStack().pop();
+        if (activeStack().isEmpty()) {
+            shrink();
+        }
+        return poppedElement;
+    }
+
+    private void shrink() {
+        stackList.remove(activeStackIndex);
+        --activeStackIndex;
+    }
+
+    public int peek() {
+        if (activeStack().isEmpty()) {
+            throw new UnderflowException();
+        }
+        return activeStack().peek();
+    }
+
+    public boolean isEmpty() {
+        return activeStack().isEmpty();
+    }
+
+    private LinkedList<Integer> activeStack() {
+        return stackList.get(activeStackIndex);
+    }
+
+    public static class UnderflowException extends RuntimeException {}
+}
---
+++ src/test/java/io/fenske/crackinginterview/chapter3/SetOfStacksTest.java
@@ -0,0 +1,39 @@
+package io.fenske.crackinginterview.chapter3;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SetOfStacksTest {
+
+    private SetOfStacks setOfStacks;
+
+    @BeforeEach
+    void setUp() {
+        setOfStacks = new SetOfStacks(1);
+    }
+
+    @Test
+    public void shouldUnderflowWhenPopEmpty() {
+        assertThrows(SetOfStacks.UnderflowException.class, () -> setOfStacks.pop());
+    }
+
+    @Test
+    void shouldPeek() {
+        setOfStacks.push(1);
+        setOfStacks.push(2);
+        setOfStacks.pop();
+        assertEquals(1, setOfStacks.peek());
+    }
+
+    @Test
+    void shouldCheckIfEmpty() {
+        setOfStacks.push(1);
+        setOfStacks.push(1);
+        setOfStacks.pop();
+        assertFalse(setOfStacks.isEmpty());
+    }
+}
