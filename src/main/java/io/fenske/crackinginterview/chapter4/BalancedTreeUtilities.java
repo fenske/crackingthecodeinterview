eed5809596cdd925b134da8c7fdbc94230954f0a
Anton Fenske <anton@fenske.io>
2017-03-20 20:21:58 +0100

    Implement search of the lowest common ancestor

--- src/main/java/io/fenske/crackinginterview/chapter4/BalancedTreeUtilities.java
+++ src/main/java/io/fenske/crackinginterview/chapter4/BalancedTreeUtilities.java
@@ -21,4 +21,29 @@ public class BalancedTreeUtilities {
         }
         return max(findMaxDepth(node.left), findMaxDepth(node.right)) + 1;
     }
+
+    public static TreeNode findLowestCommonAncestor(TreeNode root, TreeNode x, TreeNode y) {
+        if (isCommonAncestor(root.left, x, y)) {
+            return findLowestCommonAncestor(root.left, x, y);
+        } else if (isCommonAncestor(root.right, x, y)) {
+            return findLowestCommonAncestor(root.right, x, y);
+        } else if (isCommonAncestor(root, x, y)) {
+            return root;
+        }
+        return null;
+    }
+
+    private static boolean isCommonAncestor(TreeNode root, TreeNode x, TreeNode y) {
+        return isAncestor(root, x) && isAncestor(root, y);
+    }
+
+    private static boolean isAncestor(TreeNode currentNode, TreeNode searchedNode) {
+        if (currentNode == null) {
+            return false;
+        }
+        if (currentNode.equals(searchedNode)) {
+            return true;
+        }
+        return isAncestor(currentNode.left, searchedNode) || isAncestor(currentNode.right, searchedNode);
+    }
 }
--- src/test/java/io/fenske/crackinginterview/chapter4/BalancedTreeUtilitiesTest.java
+++ src/test/java/io/fenske/crackinginterview/chapter4/BalancedTreeUtilitiesTest.java
@@ -1,6 +1,7 @@
 package io.fenske.crackinginterview.chapter4;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 import org.junit.jupiter.api.Test;
@@ -28,4 +29,29 @@ public class BalancedTreeUtilitiesTest {
 
         assertTrue(BalancedTreeUtilities.isBalanced(a));
     }
+
+    @Test
+    void shouldFindLowestCommonAncestor() {
+        TreeNode a = new TreeNode();
+        TreeNode b = new TreeNode();
+        TreeNode c = new TreeNode();
+        TreeNode d = new TreeNode();
+        TreeNode e = new TreeNode();
+        TreeNode f = new TreeNode();
+        TreeNode g = new TreeNode();
+        TreeNode h = new TreeNode();
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        c.right = f;
+        e.left = g;
+        f.left = h;
+
+        assertEquals(b, BalancedTreeUtilities.findLowestCommonAncestor(a, d, g));
+        assertEquals(a, BalancedTreeUtilities.findLowestCommonAncestor(a, d, h));
+    }
+
+
 }
