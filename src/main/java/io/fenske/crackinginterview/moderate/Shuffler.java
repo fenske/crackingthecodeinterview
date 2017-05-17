ed820a44ec10d8da54a6040d3d4b9640eb91eb3e
Anton Fenske <anton@fenske.io>
2017-05-17 20:25:39 +0200

    Get a random subset

--- src/main/java/io/fenske/crackinginterview/moderate/Shuffler.java
+++ src/main/java/io/fenske/crackinginterview/moderate/Shuffler.java
@@ -5,18 +5,33 @@ import java.util.Random;
 
 public class Shuffler {
   public static void shuffle(int[] cards) {
-    Random randomizer = new Random(System.nanoTime());
     for (int i = 0; i < cards.length; i++) {
-      int position = randomizer.nextInt(cards.length - i) + i;
-      int swap = cards[position];
-      cards[position] = cards[i];
-      cards[i] = swap;
+      swap(cards, i, getRandomInteger(i, cards.length));
     }
+  }
+
+  public static int[] getRandomSubset(int[] arr, int m) {
+    int[] copy = Arrays.copyOf(arr, arr.length);
+    for (int i = 0; i < m; i++) {
+      swap(copy, i, getRandomInteger(i, copy.length));
+    }
+    return Arrays.copyOf(copy, m);
+  }
+
+  private static void swap(int[] arr, int i, int j) {
+    int swap = arr[j];
+    arr[j] = arr[i];
+    arr[i] = swap;
+  }
+
+  private static int getRandomInteger(int lo, int hi) {
+    return new Random(System.nanoTime()).nextInt(hi - lo) + lo;
   }
 
   public static void main(String[] args) {
     int[] cards = {0,1,2,3,4,5};
     shuffle(cards);
     System.out.println(Arrays.toString(cards));
+    System.out.println(Arrays.toString(getRandomSubset(cards, 3)));
   }
 }
