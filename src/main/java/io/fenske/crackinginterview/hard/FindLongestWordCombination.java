5693134b1e38e02e330d02487072c352516f1e82
Anton Fenske <anton@fenske.io>
2017-05-23 20:59:45 +0200

    Refactor

--- src/main/java/io/fenske/crackinginterview/hard/FindLongestWordCombination.java
+++ src/main/java/io/fenske/crackinginterview/hard/FindLongestWordCombination.java
@@ -1,24 +1,29 @@
 package io.fenske.crackinginterview.hard;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 public class FindLongestWordCombination {
   public static String getLongestWordCombination(String[] words) {
     Set<String> dictionary = new HashSet<>();
-    for (String word : words) {
-      dictionary.add(word);
-    }
+    Collections.addAll(dictionary, words);
     Arrays.sort(words, (s1, s2) -> sizeOrientedCompare(s1, s2));
     for (String word : words) {
       for (int i = 1; i < word.length() - 1; i++) {
-        if (dictionary.contains(word.substring(i)) && dictionary.contains(word.substring(i, word.length()))) {
+        if (isValidPair(dictionary, word, i)) {
           return word;
         }
       }
     }
     throw new IllegalArgumentException("Should never get here.");
+  }
+
+  private static boolean isValidPair(Set<String> dictionary, String word, int boundary) {
+    String first = word.substring(boundary);
+    String second = word.substring(boundary, word.length());
+    return dictionary.contains(first) && dictionary.contains(second);
   }
 
   private static int sizeOrientedCompare(String s1, String s2) {
