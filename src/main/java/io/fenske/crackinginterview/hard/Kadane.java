73e2345c019a0c7236b42c46be5b5be31b2d29fe
Anton Fenske <anton@fenske.io>
2017-06-03 21:51:19 +0200

    Implement finding of max submatrix

--- src/main/java/io/fenske/crackinginterview/hard/Kadane.java
+++ src/main/java/io/fenske/crackinginterview/hard/Kadane.java
@@ -12,4 +12,26 @@ public class Kadane {
     }
     return maxGlobal;
   }
+
+
+  public static int getMaxSubmatrixSum(int[][] matrix) {
+    int width = matrix[0].length;
+    int height = matrix.length;
+    int currentResult;
+    int maxSum = Integer.MIN_VALUE;
+
+    for (int leftColumn = 0; leftColumn < width; leftColumn++) {
+      int[] tmp = new int[height];
+      for (int rightColumn = leftColumn; rightColumn < width; rightColumn++) {
+        for (int i = 0; i < height; i++) {
+          tmp[i] += matrix[i][rightColumn];
+        }
+        currentResult = getMaxElementSum(tmp);
+        if (currentResult > maxSum) {
+          maxSum = currentResult;
+        }
+      }
+    }
+    return maxSum;
+  }
 }
--- src/test/java/io/fenske/crackinginterview/hard/KadaneTest.java
+++ src/test/java/io/fenske/crackinginterview/hard/KadaneTest.java
@@ -1,8 +1,10 @@
 package io.fenske.crackinginterview.hard;
 
+import static io.fenske.crackinginterview.hard.Kadane.getMaxElementSum;
+import static io.fenske.crackinginterview.hard.Kadane.getMaxSubmatrixSum;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class KadaneTest {
@@ -10,6 +12,17 @@ public class KadaneTest {
   @Test
   public void shouldFindMaxSumSubarray() {
     int[] arr = {1, -3, 2, 1, -1};
-    Assert.assertThat(Kadane.getMaxElementSum(arr), equalTo(3));
+    assertThat(getMaxElementSum(arr), equalTo(3));
+  }
+
+  @Test
+  public void shouldFindMaxSubmatrix() {
+    int[][] matrix = new int[][] {
+        {1, 2, -1, -4, -20},
+        {-8, -3, 4, 2, 1},
+        {3, 8, 10, 1, 3},
+        {-4, -1, 1, 7, -6}
+    };
+    assertThat(getMaxSubmatrixSum(matrix), equalTo(29));
   }
 }
